2819a1e9635be2a271042b2cebf9326881b8b8b5
jonathanmusila <jonathanmusila6@gmail.com>
2020-01-20 17:34:26 +0300

    added customer class

---
+++ Customer.php
@@ -0,0 +1,23 @@
+<? php
+
+/**
+ * PRIVATE
+ * The private visibility means that the properties and methods of the class can 
+ * not be accesses by outside
+ * 
+ * PROTECTED 
+ * The protected visibility means that the properties and methods of the class can 
+ * be accessed only by its children class
+ * 
+ * PUBLIC
+ * The public visibility means that the properties and methods of the class can 
+ * be accessed by outside
+ * 
+ */
+  
+class Customer {
+    private $id;
+    private $firstname;
+    private $surname;
+    private $email;
+}
