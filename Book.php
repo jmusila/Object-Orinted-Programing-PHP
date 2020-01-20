3832db59b211fc041ec75ad4262aee7fcfe3aa75
jonathanmusila <jonathanmusila6@gmail.com>
2020-01-20 17:14:13 +0300

    added type and default

--- Book.php
+++ Book.php
@@ -1,4 +1,6 @@
 <? php
+// default arguments 
+// type hinting 
 
 class Book {
     //properties or class variables
@@ -8,11 +10,21 @@ class Book {
     public $available;
 
     // magic method
-    public function __construct($isbn, $title, $author, $available){
+    public function __construct(int $isbn, string $title, string $author, int $available){
         $this->isbn = $isbn;
         $this->title = $title;
         $this->author = $author;
         $this->available = $available;
+    }
+
+    // another magic method
+    // converts class properties to string 
+    public function __toString() {
+        $result = $this->title . ' by '. $this->author;
+        if (!$this->available){
+            $result .= 'Not Available';
+        }
+        return $result;
     }
 
     // methods are functions
@@ -39,10 +51,4 @@ class Book {
 $harry_potter = new Book(6576586, "Harry Potter and the Magicians", "John Doe", 10);
 // removed the direct instantiation since we are now using our magic method
 
-if ($harry_potter->getCopy()) {
-    echo 'Here is your copy of ' . $harry_potter->title. '<br>';
-}else {
-    echo 'Sorry its gone!';
-}
-
-var_dump($harry_potter);
+echo $harry_potter;
